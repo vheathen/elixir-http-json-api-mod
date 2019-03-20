90b13876b5b17f8e48ab1872fae4aaf2cc706de4
heathen <v.heathen@gmail.com>
2019-03-20 17:35:41 +0500

    Готовый проект до развёртывания

---
+++ lib/minimal_server/endpoint.ex
@@ -0,0 +1,30 @@
+defmodule MinimalServer.Endpoint do
+  use Plug.Router
+
+  plug(:match)
+
+  plug(Plug.Parsers,
+    parsers: [:json],
+    pass: ["application/json"],
+    json_decoder: Poison
+  )
+
+  plug(:dispatch)
+
+  forward("/bot", to: MinimalServer.Router)
+
+  match _ do
+    send_resp(conn, 404, "Requested page not found!")
+  end
+
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [opts]}
+    }
+  end
+
+  def start_link(_opts),
+    do: Plug.Cowboy.http(__MODULE__, [])
+
+end
---
+++ mix.exs
@@ -0,0 +1,31 @@
+defmodule MinimalServer.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :minimal_server,
+      version: "0.1.0",
+      elixir: "~> 1.8",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {MinimalServer.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:poison, "~> 3.0"},
+      {:plug, "~> 1.7"},
+      {:cowboy, "~> 2.5"},
+      {:plug_cowboy, "~> 2.0"}
+    ]
+  end
+end
